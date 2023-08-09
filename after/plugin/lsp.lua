61ac5a22a36e26ffaa280af1ba409ab999b09d9e
Ya-maz <yamaz.job@gmail.com>
2023-08-09 22:25:14 +0300

    prettier intall via packer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,18 +15,18 @@ local status, nvim_lsp = pcall(require, "lspconfig")
 if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
-
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
-local enable_format_on_save = function(_, bufnr)
-    vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup_format,
-        buffer = bufnr,
-        callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-    })
-end
+--
+--local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+--local enable_format_on_save = function(_, bufnr)
+--    vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
+--    vim.api.nvim_create_autocmd("BufWritePre", {
+--        group = augroup_format,
+--        buffer = bufnr,
+--        callback = function()
+--            vim.lsp.buf.format({ bufnr = bufnr })
+--        end,
+--    })
+--end
 
 -- Run gofmt on save
 --
